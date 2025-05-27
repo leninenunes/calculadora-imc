164c24a6f60656e7cf2fb0cf4a13e4e4a457a0ea
Lenine Nunes <lenine.michel@gmail.com>
2025-05-26 23:02:59 -0300

    Identificacao dos elementros e click para limpar

--- app/src/main/java/com/example/calculadoraimc/MainActivity.java
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.calculadoraimc;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,19 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        EditText editTextWeight = findViewById(R.id.edittext_weight);
+        SeekBar seekBarHeight = findViewById(R.id.seekbar_height);
+        Button buttonClear = findViewById(R.id.button_clear);
+        Button buttonCalculate = findViewById(R.id.button_calculate);
+
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextWeight.setText("");
+                seekBarHeight.setProgress(0);
+            }
         });
     }
 }
